ed7c84b6d09076a0a26d2b63f05d15d31474e14a
Bradley Miller <bm17159@ms.wasatch.edu>
2024-01-17 14:32:34 -0700

    Day 2
    Set up random Spawning and forces

--- Assets/Scripts/DebrisController.cs
+++ Assets/Scripts/DebrisController.cs
@@ -1,31 +1,47 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class DebrisController : MonoBehaviour
 {
-    [Header("Debris Model")]
-    public GameObject Debris;
+    [Header("Debris Model")] 
+    public GameObject debrisModel;
 
-    [Header("Debris Spawner (At Origin)")]
-    [SerializeField] private Transform SpawnerPos;
+    
+    [Header("Spawning Info")]
     [SerializeField] private int Range = 10;
-    
-    
-    
-    
+    [SerializeField] private int amountToSpawn = 25;
 
+    private Transform[] debris;
+    
     // Start is called before the first frame update
     void Start()
     {
-        SpawnerPos = this.transform;
-        transform.position = Random.insideUnitSphere * Range;
+        debris = new Transform[amountToSpawn];
+        CreateDebris();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void CreateDebris()
+    {
+        
+        
+        for (int i = 0; i <= amountToSpawn; i++)
+        {
+            Vector3 pos = (Random.insideUnitSphere * Range) + transform.position;
+            GameObject temp = Instantiate(debrisModel, pos, transform.rotation, transform);
+            Rigidbody rb = temp.GetComponent<Rigidbody>();
+            rb.AddForce(0, 0, 1);
+            debris[i] = temp.transform;
+        }
     }
 }
