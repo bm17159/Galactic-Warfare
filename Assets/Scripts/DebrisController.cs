1a6c58dea895cc1c360f0205ac959e429fecf697
Bradley Miller <bm17159@ms.wasatch.edu>
2024-01-19 14:27:59 -0700

    Day 3
    Finished Basic Debris Spawner

--- Assets/Scripts/DebrisController.cs
+++ Assets/Scripts/DebrisController.cs
@@ -7,41 +7,52 @@ using Random = UnityEngine.Random;
 
 public class DebrisController : MonoBehaviour
 {
-    [Header("Debris Model")] 
-    public GameObject debrisModel;
+    [Header("Debris Model")] public GameObject debrisModel;
 
-    
-    [Header("Spawning Info")]
-    [SerializeField] private int Range = 10;
+
+    [Header("Spawning Info")] [SerializeField]
+    private int Range = 10;
+
     [SerializeField] private int amountToSpawn = 25;
-
     private Transform[] debris;
-    
+
+    [Header("Force Applied")] [SerializeField]
+    private int forceMultiplier = 10;
+
+
     // Start is called before the first frame update
     void Start()
     {
         debris = new Transform[amountToSpawn];
         CreateDebris();
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      
     }
 
     private void CreateDebris()
     {
-        
-        
-        for (int i = 0; i <= amountToSpawn; i++)
+
+        // Create and position 25 Debris in a radius
+        for (int i = 0; i < amountToSpawn; i++)
         {
+            // Calculate Position of Next Debris and Force
             Vector3 pos = (Random.insideUnitSphere * Range) + transform.position;
+            Vector3 force = new Vector3(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f),
+                Random.Range(-10.0f, 10.0f));
+            // Spawn the new Debris
             GameObject temp = Instantiate(debrisModel, pos, transform.rotation, transform);
+            // Add a random force to each Debris
             Rigidbody rb = temp.GetComponent<Rigidbody>();
-            rb.AddForce(0, 0, 1);
+            rb.AddForce(force * forceMultiplier);
+            // Store in Array
             debris[i] = temp.transform;
         }
     }
+
 }
+    
