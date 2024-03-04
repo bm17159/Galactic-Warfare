4639bd94fbaa5e1939603ad5d9ff651bdb44afa1
rome <rp25563@ms.wasatch.edu>
2024-03-04 12:30:23 -0700

    player prefabs
    set firing points on prefabs

--- Assets/Scripts/Controllers/Player Controller.cs
+++ Assets/Scripts/Controllers/Player Controller.cs
@@ -2,11 +2,13 @@ using System;
 using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
 using System.Collections;
+using System.Diagnostics;
 using Unity.Netcode;
 using UnityEngine;
 using Cinemachine;
 
 using Cursor = UnityEngine.Cursor;
+using Debug = UnityEngine.Debug;
 
 public class PlayerController : NetworkBehaviour
 {
@@ -17,27 +19,54 @@ public class PlayerController : NetworkBehaviour
     [SerializeField] private float brakeSpeed;
 
     [SerializeField] private float rotateSpeed;
+    
+    [SerializeField] private string Class;
 
     [SerializeField] private CinemachineFreeLook fl;
     //[SerializeField] private AudioListener listener;
 
-    //private InputManager inputManager;
+    private Stopwatch timemoved;
+
+    private InputManager inputManager;
     private CharacterController controller;
     public GameObject follow;
     public Rigidbody rb;
     private Vector3 rotation;
-    //private Camera c;
 
     private bool thrustDown = false;
     private bool rotateDown = false;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
-        c = GetComponent<Rigidbody>();
-        
+
         Cursor.lockState = CursorLockMode.Locked;
         rotation = new Vector3(0, 0, rotateSpeed);
-        //c = GetComponent<Camera>();
+
+        /*#region vars
+
+        if (Class.Equals("Fighter"))
+        {
+            maxSpeedf = 200;
+            brakeTime = 1.25f;
+            brakeSpeed = -200;
+            rotateSpeed = 50;
+        }
+        if (Class.Equals("Heavy"))
+        {
+            maxSpeedf = 200;
+            brakeTime = 1.25f;
+            brakeSpeed = -200;
+            rotateSpeed = 50;
+        }if (Class.Equals("Speedster"))
+        {
+            maxSpeedf = 200;
+            brakeTime = 1.25f;
+            brakeSpeed = -200;
+            rotateSpeed = 50;
+        }
+
+        #endregion*/
     }
 
     public override void OnNetworkSpawn()
@@ -54,10 +83,9 @@ public class PlayerController : NetworkBehaviour
 
     void FixedUpdate()
     {
-        if (!IsOwner)return;
-        
+        if (!IsOwner) return;
         Thrust(Time.deltaTime);
-        Rotate();
+        //Rotate();
         rb.transform.LookAt(follow.transform);
         //transform.rotation = Quaternion.Lerp();
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -65,71 +93,91 @@ public class PlayerController : NetworkBehaviour
             Cursor.lockState = CursorLockMode.None;
         }
     }
-    #region Thrust 
-    private void OnThrustCanceled(InputAction.CallbackContext obj)
+
+    #region Thrust
+    void OnThrustPerformed(InputAction.CallbackContext obj)
     {
-        
-        if (rb.velocity.magnitude > 0f)
+        if (!IsOwner) return;
+            if (rb.velocity.magnitude > 0f)
+            {
+                rb.AddForce(follow.transform.forward * (20000 * Time.deltaTime));
+                //rb.velocity = new Vector3(moveDirection.x, moveDirection.y, moveDirection.z);
+                rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeedf);
+            }
+    }
+    
+
+    private void Thrust(float delta)
+    {
+        if (Input.GetKeyDown(KeyCode.W))
         {
-            if (!IsOwner)return;
+            timemoved.Start();
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
             rb.AddForce(follow.transform.forward * (20000 * delta));
             //rb.velocity = new Vector3(moveDirection.x, moveDirection.y, moveDirection.z);
             rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeedf);
         }
-
-        IEnumerator Slow()
+        else
         {
-            if (!IsOwner)return;
+            
             if (rb.velocity.magnitude > 0f)
             {
                 StartCoroutine(Slow());
             }
 
-    private void OnThrustPerformed(InputAction.CallbackContext obj)
-    {
-        Debug.Log("forward");
-        thrustDown = true;
-    }
-    
-   
-    private void Thrust()
-    {
-       
-            rb.AddForce(follow.transform.forward * (20000 * Time.deltaTime));
-            //rb.velocity = new Vector3(moveDirection.x, moveDirection.y, moveDirection.z);
-            rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeedf);
-
-           
+            IEnumerator Slow()
+            {
+                rb.AddForce(follow.transform.forward * (brakeSpeed * delta));
+                if (timemoved.ElapsedMilliseconds * 100 > brakeTime)
+                {
+                    yield return new WaitForSeconds(brakeTime);
+                }
+                else
+                {
+                    yield return new WaitForSeconds(timemoved.ElapsedMilliseconds * 100);
+                }
+                timemoved.Stop();
+                rb.velocity -= rb.velocity;
+                
+            }
             
         }
-    #endregion
 
-    #region Rotate
-    private void OnRotateCanceled(InputAction.CallbackContext obj)
-    {
-        rotateDown = false;
     }
+            #endregion
 
-    private void OnRotatePerformed(InputAction.CallbackContext obj)
-    {
-        Debug.Log("rotate");
-        rotateDown = true;
-    }
+            /*#region Rotate
 
-    private void Rotate()
-    {
-        if (!IsOwner)return;
-        if (Input.GetKey(KeyCode.A))
-        {
-            Quaternion deltaRotation = Quaternion.Euler(rotation * Time.fixedDeltaTime);
-            rb.MoveRotation(rb.rotation * deltaRotation);
+            void OnRotateCanceled(InputAction.CallbackContext obj)
+            {
+                rotateDown = false;
+            }
+
+            void OnRotatePerformed(InputAction.CallbackContext obj)
+            {
+                Debug.Log("rotate");
+                rotateDown = true;
+            }
+
+            void Rotate()
+            {
+                if (!IsOwner) return;
+                if (Input.GetKey(KeyCode.A))
+                {
+                    Quaternion deltaRotation = Quaternion.Euler(rotation * Time.fixedDeltaTime);
+                    rb.MoveRotation(rb.rotation * deltaRotation);
+                }
+
+                if (Input.GetKey(KeyCode.D))
+                {
+                    Quaternion deltaRotation = Quaternion.Euler(-rotation * Time.fixedDeltaTime);
+                    rb.MoveRotation(rb.rotation * deltaRotation);
+                }
+            }
+
+            #endregion*/
         }
 
-        if (Input.GetKey(KeyCode.D))
-        {
-            Quaternion deltaRotation = Quaternion.Euler(-rotation * Time.fixedDeltaTime);
-            rb.MoveRotation(rb.rotation * deltaRotation);
-        }
-    }
-}
 
--- Assets/Scripts/Multiplayer/NetworkManagerUI.cs
+++ Assets/Scripts/Multiplayer/NetworkManagerUI.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
+using UnityEngine.Assertions.Must;
 using UnityEngine.UI;
 using UnityEngine.Rendering;
 
@@ -12,20 +13,33 @@ public class NetworkManagerUI : MonoBehaviour
     [SerializeField] private Button serverBtn;
     [SerializeField] private Button hostBtn;
     [SerializeField] private Button clientBtn;
+    [SerializeField] private GameObject fighter;
+    [SerializeField] private GameObject heavy;
+    [SerializeField] private GameObject speedster;
 
     private void Awake()
     {
         serverBtn.onClick.AddListener((() =>
         {
             NetworkManager.Singleton.StartServer();
+            serverBtn.gameObject.SetActive(false);
+            hostBtn.gameObject.SetActive(false);
+            clientBtn.gameObject.SetActive(false);
         }));
         hostBtn.onClick.AddListener((() =>
         {
+            //NetworkManager.Singleton
             NetworkManager.Singleton.StartHost();
+            serverBtn.gameObject.SetActive(false);
+            hostBtn.gameObject.SetActive(false);
+            clientBtn.gameObject.SetActive(false);
         }));
         clientBtn.onClick.AddListener((() =>
         {
             NetworkManager.Singleton.StartClient();
+            serverBtn.gameObject.SetActive(false);
+            hostBtn.gameObject.SetActive(false);
+            clientBtn.gameObject.SetActive(false);
         }));
         
     }
